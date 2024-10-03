755a01eccd8a669655bf1120fcada320807a6033
takapon-design <yd64tk94@gmail.com>
2024-10-03 20:20:22 +0900

    フォルダをアップロード

---
+++ JavaScript-basic/js/constant.js
@@ -0,0 +1,11 @@
+// 定数の宣言・値の代入
+const shippingfee = 500;
+
+// コンソールへの出力
+console.log(shippingfee);
+
+// 値の再代入（エラー）
+shippingfee = 800;
+
+// コンソールへの出力
+console.log(shippingfee);
---
+++ JavaScript-basic/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log(100);
+console.log(-10);
+console.log(2.5);
